982961176f2a7a679281f5bf352740d076bcfaa7
William Park <wjypark94@gmail.com>
2018-04-25 15:29:04 -0700

    set up parent component and basic set up

---
+++ src/components/game.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Game extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>
+                    Hello Hot and Cold Game
+                </h1>
+            </div>
+        )
+    }
+}
