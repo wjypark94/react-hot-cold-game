e31c4eb10c746536c71eaf75c0e63358215a73ce
William Park <wjypark94@gmail.com>
2018-04-25 16:42:42 -0700

    added guess section and feedback components

--- src/components/game.js
+++ src/components/game.js
@@ -4,7 +4,19 @@ import Header from './header';
 import GuessSection from './guess-section';
 
 export default class Game extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            guesses: [],
+            feedback: 'Make your guess!',
+            auralStatus: '',
+            correctAnswer:  Math.round(Math.random() * 100) + 1
+        };
+        
+    }
     render(){
+        const {feedback, guesses, auralStatus} = this.state;
+        const guessCount = guesses.length; 
         return (
             <div>
                 <Header/>
--- src/components/guess-section.js
+++ src/components/guess-section.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import GuessForm from './guess-form';
+import Feedback from './feedback';
 
 export default function GuessSection(props){
+    const {feedback, guessCount} = props;
     return (
         <section>
+            <Feedback feedback={feedback} guessCount={guessCount} />
             <GuessForm/>
         </section>
     )
